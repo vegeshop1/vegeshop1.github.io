7b9f60bddf3ec0142ffd30adab1e52f8597188c6
Nitish <pqr.nitish@gmail.com>
2020-10-21 12:33:40 +0530

    vegeshop1

---
+++ scripts/script1.js
@@ -0,0 +1,13 @@
+var i=0;
+function toggleBar(){
+    var x=document.getElementById('menu2');
+    if(i%2==0){
+        x.style.top="0";
+        i++;
+    }
+    else{
+        x.style.top="-1000px";
+        i++;
+    }
+    console.log(i);
+}
